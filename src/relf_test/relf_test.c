5d9e756153818634e9bddfc0dac511bb4c649751
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-08-25 20:31:36 +0300

    add dlsym to actually check if my shit works

--- src/relf_test/relf_test.c
+++ src/relf_test/relf_test.c
@@ -1,4 +1,5 @@
 #include "relf/relf.h"
+#include <dlfcn.h>
 #include <stdio.h>
 
 int main(int argc, char** argv)
@@ -8,5 +9,13 @@ int main(int argc, char** argv)
 
     printf("image %p\n", dummy.image);
     printf("type %u\n", dummy.type);
+
+    relf_close(&dummy);
+
+    // dlopen
+    void* so = dlopen(argv[1], RTLD_NOW);
+    printf("dummy_function1 %p\n", dlsym(so, "dummy_function1"));
+    dlclose(so);
+    
     return 0;
 }
